858e58e1da9ef9dadefd082dc4be5027c5bc6d7d
aidaidaid <aidashirinovaaa@gmail.com>
2021-09-10 16:08:29 +0400

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import NoteList from "./components/NoteList";
+import NoteItem from './components/noteItem'
+import '../src/App.css';
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      this.state = {
+        id: 1,
+        notes: [],
+        titleValue: '',
+        contentValue: '',
+        noteToEdit: [],
+        idToEdit: 0,
+        isEdit: false,
+      } 
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  addNote = (titleValue, contentValue) => {
+    this.setState(() => {
+      return {
+      id: this.state.id+1,
+      notes: [...this.state.notes, {title: titleValue, content: contentValue, id: this.state.id}],
+    }
+    })
+  };
+ 
+  saveNote = () => {
+    const newNotes = this.state.notes.map((item, index)=>{
+        if (item.id===this.state.idToEdit){
+          return {
+            ...item,
+            title: this.state.titleValue,
+            content: this.state.contentValue
+          }
+        }
+        return item;
+    });
+    this.setState({
+      notes: newNotes,
+      idToEdit: 0,
+      titleValue: '',
+      contentValue: '',
+      isEdit: false,
+    })
+  }
+
+  deleteNote = (idToDelete) => {
+    this.setState({notes: this.state.notes.filter((note)=>note.id!==idToDelete)})
+  }
+
+  editNote = (idToEdit) => {
+    const noteToEd =  this.state.notes.find((note)=>note.id===idToEdit)
+    this.setState({noteToEdit: [noteToEd]});
+    this.setState({idToEdit: idToEdit});
+    this.setState(prevState => ({
+      ...prevState,
+      titleValue: noteToEd?.title,
+      contentValue: noteToEd?.content,
+    }))
+    this.setState({isEdit: true})
+  }
+
+  onChangeTitle = (text) => {
+    this.setState({titleValue: text});
+  }
+
+  onChangeContent = (text) => {
+    this.setState({contentValue: text});
+  }
+
+  createNote = () => {
+    this.addNote(this.state.titleValue, this.state.contentValue);
+    this.setState({titleValue: ''});
+    this.setState({contentValue: ''});
+   }
+
+  render(){  
+    return(
+      <div className='app'>
+        <NoteItem
+          createNote={this.createNote}
+          onChangeTitle={this.onChangeTitle}
+          onChangeContent={this.onChangeContent}
+          titleValue={this.state.titleValue}
+          contentValue={this.state.contentValue}
+          isEdit={this.state.isEdit}
+          addNote={this.addNote}
+          saveNote={this.saveNote}
+          noteToEdit={this.state.noteToEdit}
+        />
+        <NoteList notes={this.state.notes} editNote={this.editNote} deleteNote={this.deleteNote}/>
+      </div>
+    )
+  }
 }
 
 export default App;
